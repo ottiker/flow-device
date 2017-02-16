0cee721e0eeef16667f3640310a275b8985f8a13
Adrian Ottiker <adrian@ottiker.com>
2017-02-16 18:04:17 +0100

    fix paths

--- index.js
+++ index.js
@@ -2,19 +2,21 @@
 
 const fs = require("fs");
 const path = require("path");
-const base = "/sys/class/gpio/gpio"
+const base = "/sys/class/gpio/"
 
-exports.activate = (gpio) => {
-    fs.createWriteStream(base + 'export').end(gpio.toString())
-}; 
-exports.deactivate= (gpio) => {
-    fs.createWriteStream(base + 'unexport').end(gpio.toString())
+exports.activate = (gpio, direction) => {
+    fs.createWriteStream(base + "export").end(gpio.toString())
+    fs.createWriteStream(base + "gpio" + gpio + "/direction").end(direction.toString())
+};
+
+exports.deactivate = (gpio) => {
+    fs.createWriteStream(base + "unexport").end(gpio.toString())
 }; 
 
 exports.read = (gpio) => {
-    return fs.createReadStream(path.resolve(base + gpio + "/value"));
+    return fs.createReadStream(path.resolve(base + "gpio" + gpio + "/value"));
 };
 
 exports.write = (gpio) => {
-    return fs.createWriteStream(path.resolve(base + gpio + "/value"));
+    return fs.createWriteStream(path.resolve(base + "gpio" + gpio + "/value"));
 };
