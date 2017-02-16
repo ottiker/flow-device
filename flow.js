8c885bdca950df204b7a906912afcb8a50f6444c
Adrian Ottiker <adrian@ottiker.com>
2017-02-16 17:45:04 +0100

    debug logs

--- flow.js
+++ flow.js
@@ -3,20 +3,24 @@
 const gpio = require("./index");
 
 exports.activate = (scope, state, args, stream, next) => {
+    console.log('GPIO.activate:', args);
     gpio.activate(args.gpio);
     next(null, data, stream);
 };
 
 exports.deactivate = (scope, state, args, stream, next) => {
+    console.log('GPIO.deactivate:', args);
     gpio.deactivate(args.gpio);
     next(null, data, stream);
 };
 
 exports.read = (scope, state, args, data, stream, next) => {
+    console.log('GPIO.read:', args);
     next(null, data, gpio.read(args.path));
 };
 
 exports.write = (scope, state, args, data, stream, next) => {
+    console.log('GPIO.write:', args);
     stream.pipe(gpio.write(args.path));
     next(null, data, stream);
 };
